2c9c671591f524ca4e8dc30c7e9d73447418e371
huyj2ee <huyj2ee@outlook.com>
2024-07-29 13:12:34 +0700

    refactor change signout to use PUT method instead of POST method.

--- src/main/java/com/blogspot/huyj2ee/jwt/jwtutils/controllers/v1/JwtController.java
+++ src/main/java/com/blogspot/huyj2ee/jwt/jwtutils/controllers/v1/JwtController.java
@@ -12,6 +12,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,7 +53,7 @@ public class JwtController {
   private RefreshTokenService refreshTokenService;
 
   @PreAuthorize("isAuthenticated()")
-  @PostMapping("/signout")
+  @PutMapping("/signout")
   public ResponseEntity<?> logOut() throws Exception {
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
     UserPrincipal userDetails = (UserPrincipal) authentication.getPrincipal();
