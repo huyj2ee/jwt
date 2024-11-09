3537cf4f62e52dd5978b9543e1085f7e10613aa4
huyj2ee <huyj2ee@outlook.com>
2024-11-09 16:06:09 +0700

    [backend] create initial Spring project

---
+++ backend/src/test/java/com/blogspot/huyj2ee/jwt/JwtApplicationTests.java
@@ -0,0 +1,11 @@
+package com.blogspot.huyj2ee.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+  @Test
+  void contextLoads() {
+  }
+}
