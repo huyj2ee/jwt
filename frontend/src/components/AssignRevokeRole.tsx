55381a171f1acfe97ddb1d339f8dd27c5eacb8e3
huyj2ee <huyj2ee@outlook.com>
2025-06-10 15:35:24 +0700

    [frontend] decorate AssignRevokeRole component

--- frontend/src/components/AssignRevokeRole.tsx
+++ frontend/src/components/AssignRevokeRole.tsx
@@ -5,6 +5,8 @@ import { RolesStore } from '../app/api';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../app/store';
 import { assignRevokeRoleAsync, getAllRolesAsync, getAssignedRolesAsync } from './roles/rolesSlice';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
 
 const Roles : React.FunctionComponent<{roles: RolesStore}> = ({roles}: {roles: RolesStore}) => {
   const dispatch:AppDispatch = useDispatch();
@@ -22,9 +24,10 @@ const Roles : React.FunctionComponent<{roles: RolesStore}> = ({roles}: {roles: R
         roles.roles === null || roles.assignedRoles === null ? '' :
         roles.roles.map(
           (role) =>
-            <div>
-              <label htmlFor={role}>{role}</label>
+            <div className='h-[24px]'>
+              <label className='min-w-[406px] inline-block' htmlFor={role}>{role}</label>
               <input
+                className='cursor-pointer'
                 id={role}
                 type='checkbox'
                 checked={roles.assignedRoles.includes(role)}
@@ -50,10 +53,26 @@ const AssignRevokeRole : React.FunctionComponent = () => {
 
   return (
     <Layout>
-      {username} roles
-      <Roles roles={roles}/>
-      {roles.errorMessage === null ? null : <div>Unexpected Error has occurred.</div>}
-      <Link to="/users">Back</Link>
+`      <div className='flex justify-center items-center h-[calc(100%-50px)] w-full'>
+        <div className='box-content relative w-[524px] bg-[#eeeeee] border-[#999999] border-[1px] rounded-tl-[10px] rounded-tr-[10px]'>
+          <div className='flex items-center justify-center w-[524px] h-[55px] bg-[#d9d9d9] border-[#999999] border-b-[1px] rounded-tl-[10px] rounded-tr-[10px] text-[24px]'>
+            {username} roles
+          </div>
+          <div className='flex justify-center items-center mt-[20px]'>
+            <div>
+              <Roles roles={roles}/>
+            </div>
+          </div>
+          {roles.errorMessage === null ? null : (
+            <div className='flex items-center w-[418px] h-[32px] mt-[11px] mb-[32px] ml-[53px] bg-[#f9e4dd] text-[#6c0101]'>
+              <FontAwesomeIcon className='ml-[11px] mr-[13px] w-[15px] h-[17px] text-[#db2f2f]' icon={faTriangleExclamation}/> Unexpected Error has occurred.
+            </div>
+          )}
+          <div className='flex items-center justify-end w-[524px] h-[55px] mt-[25px] bg-[#fafafa] border-[#999999] border-t-[1px]'>
+            <Link className='w-[56px] h-[34px] mr-[53px] bg-[#0099cc] rounded-[6px] text-white flex items-center justify-center' to="/users">Back</Link>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
